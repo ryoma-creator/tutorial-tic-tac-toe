d0f8f414864ba9cf665d316c5524c8a657e3150b
Ryoma/Cross <ryoma111326@gmail.com>
2024-06-17 16:22:21 +0800

    Add status display to Board component to show the winner or next player

--- src/App.js
+++ src/App.js
@@ -14,7 +14,7 @@ export default function Board() {
   const [squares, setSquares] = useState(Array(9).fill(null));
 
   function handleClick(i) {
-    if (squares[i]) {
+    if (squares[i] || calculateWinner(squares)) {
       return;
     }
     const nextSquares = squares.slice();
@@ -40,15 +40,24 @@ export default function Board() {
     ];
     for (let i = 0; i < lines.length; i++) {
       const [a, b, c] = lines[i];
-      if (squares[a] && squares[a] === setSquares[b] && squares[a] === squares[c]) {
+      if (squares[a] && squares[a] === squares[b] && squares[a] === squares[c]) {
         return squares[a];
       } 
     }
     return null;
   }
 
+  const winner = calculateWinner(squares);
+  let status;
+  if (winner){
+    status = "Winner: " + winner;
+  } else {
+    status = "Next player: " + (xIsNext ? "X" : "⭕️" );
+  }
+
   return (
     <>
+      <div className="status">{status}</div>
       <div className="board-row">
         <Square value={squares[0]}　onSquareClick={ () => handleClick(0)} />
         <Square value={squares[1]}　onSquareClick={ () => handleClick(1)} />
